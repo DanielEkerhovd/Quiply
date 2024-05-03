3f50b82799d2539b8589444ebe3bea122ae925a2
Daniel Ekerhovd <115421350+DanielEkerhovd@users.noreply.github.com>
2024-05-03 21:47:13 +0200

    Finished index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.html"],
+  content: ["./**/*.html", "./**/*.js"],
   theme: {
     extend: {
       colors: {
@@ -8,7 +8,13 @@ module.exports = {
         'secondary': {
           100: '#E2E2D5',
           200: '#888883',
-        }
+        },
+        'background': '#BA9479',
+        'highlight': '#BC845D',
+        'highlight-hover': '#BE8C69',
+        'q-yellow': '#FFD290',
+        'q-yellow-hover': '#FFD699',
+
       },
       fontFamily: {
         sans: ["Raleway", "sans-serif"],
