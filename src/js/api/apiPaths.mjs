a872e09067988275d65163cb9be926ac8fc5d4ad
Daniel Ekerhovd <115421350+DanielEkerhovd@users.noreply.github.com>
2024-05-12 18:16:20 +0200

    Revert "Js2"

--- src/js/api/apiPaths.mjs
+++
@@ -1,6 +0,0 @@
-export const baseURL = 'https://api.noroff.dev/api/v1/social/'
-
-export const apiPaths = {
-   login: 'auth/login',
-   register: 'auth/register'
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.html", "./src/**/*.mjs"],
+  content: ["./**/*.html"],
   theme: {
     extend: {
       colors: {
@@ -8,19 +8,13 @@ module.exports = {
         'secondary': {
           100: '#E2E2D5',
           200: '#888883',
-        },
-        'background': '#BA9479',
-        'highlight': '#BC845D',
-        'highlight-hover': '#BE8C69',
-        'q-yellow': '#FFD290',
-        'q-yellow-hover': '#FFD699',
-
+        }
       },
       fontFamily: {
         sans: ["Raleway", "sans-serif"],
-        lato: ["Lato", "sans-serif"],
       },
+    },
   },
   plugins: [],
-  }
-};
+}
+
